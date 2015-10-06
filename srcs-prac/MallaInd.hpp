ef35038f451e19b3b49d7f3f6cb620be3bfa12b0
JCEntrena <jentrena@correo.ugr.es>
2015-10-06 13:07:34 +0200

    Implementado el método de dibulado de mallas.

--- srcs-prac/MallaInd.hpp
+++ srcs-prac/MallaInd.hpp
@@ -1,11 +1,12 @@
 #include <Objeto3D.hpp>
+#include <tuplasg.hpp>
 #include <vector>
  
 // Clase para mallas indexadas o mallas TVT
 class MallaInd : public Objeto3D{
 protected:
     vector<Tupla3F> vertices; 
-    vector<Tupla3i> caras;     
+    vector<Tupla3i> indices;     
 
 public:
     virtual void visualizar(unsigned modo_vis);
