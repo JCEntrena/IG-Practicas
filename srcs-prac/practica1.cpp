1294186ffe88fe80e3e3912e0beeadaeafd20dbd
JCEntrena <jentrena@correo.ugr.es>
2015-10-27 12:03:04 +0100

    Cambio en tecla.

--- srcs-prac/practica1.cpp
+++ srcs-prac/practica1.cpp
@@ -6,7 +6,7 @@
 // *********************************************************************
 
 #include "aux.hpp"
-#include "tuplasg.hpp"   // Tupla3f 
+#include "tuplasg.hpp"   // Tupla3f
 #include "practica1.hpp"
 #include <vector>
 
@@ -15,28 +15,28 @@ unsigned objeto_activo = 0 ; // objeto activo: cubo (0), tetraedro (1), otros...
 // ---------------------------------------------------------------------
 // declaraciones de estructuras de datos...
 
-std::vector<MallaInd> fig; 
+std::vector<MallaInd> fig;
 
 
 // ---------------------------------------------------------------------
 // Función para implementar en la práctica 1 para inicialización.
-// Se llama una vez al inicio, cuando ya se ha creado la ventana e 
-// inicializado OpenGL. 
+// Se llama una vez al inicio, cuando ya se ha creado la ventana e
+// inicializado OpenGL.
 
 void P1_Inicializar( int argc, char *argv[] ){
 
-   Cubo cubo; 
-   Tetraedro tetraedro;   
-   fig.push_back(cubo); 
-	fig.push_back(tetraedro); 
-     
+   Cubo cubo;
+   Tetraedro tetraedro;
+   fig.push_back(cubo);
+	fig.push_back(tetraedro);
+
 }
 
 // ---------------------------------------------------------------------
 // Función invocada al pulsar una tecla con la práctica 1 activa:
 // (si la tecla no se procesa en el 'main').
 //
-//  - devuelve 'true' si la tecla se usa en esta práctica para cambiar 
+//  - devuelve 'true' si la tecla se usa en esta práctica para cambiar
 //    entre el cubo, el tetraedro u otros objetos (cambia el valor de
 //    'objeto_activo').
 //  - devuelve 'false' si la tecla no se usa en esta práctica (no ha
@@ -44,12 +44,12 @@ void P1_Inicializar( int argc, char *argv[] ){
 
 bool P1_FGE_PulsarTeclaNormal(unsigned char tecla){
 
- 	if (toupper(tecla) == 'C'){
+ 	if (toupper(tecla) == 'O'){
 	  	objeto_activo = (objeto_activo+1)%fig.size();
-       	return true;  
+       	return true;
 	}
 
-	return false; 
+	return false;
 }
 
 
@@ -59,16 +59,16 @@ bool P1_FGE_PulsarTeclaNormal(unsigned char tecla){
 
 void P1_DibujarObjetos(unsigned modo){
 
-  	fig[objeto_activo].visualizar(modo); 
+  	fig[objeto_activo].visualizar(modo);
 
 }
 
 //----------------------------------------------------------------------
-// Constructores para el Cubo y el Tetraedro. 
+// Constructores para el Cubo y el Tetraedro.
 
 Cubo::Cubo(){
-    // Inclusión de los vértices. 
-    vertices.push_back(Tupla3f(1,0,0)); 
+    // Inclusión de los vértices.
+    vertices.push_back(Tupla3f(1,0,0));
     vertices.push_back(Tupla3f(1,1,0));
     vertices.push_back(Tupla3f(0,1,0));
     vertices.push_back(Tupla3f(0,0,0));
@@ -78,29 +78,29 @@ Cubo::Cubo(){
     vertices.push_back(Tupla3f(0,0,1));
 
     // Inclusión de los índices.
-    indices.push_back(Tupla3i(7,4,3)); 
-    indices.push_back(Tupla3i(3,4,0)); 
-    indices.push_back(Tupla3i(0,1,2)); 
-    indices.push_back(Tupla3i(2,0,3)); 
-    indices.push_back(Tupla3i(3,2,6)); 
-    indices.push_back(Tupla3i(6,3,7)); 
-    indices.push_back(Tupla3i(7,6,5)); 
-    indices.push_back(Tupla3i(5,7,4)); 
-    indices.push_back(Tupla3i(4,5,1)); 
-    indices.push_back(Tupla3i(1,4,0)); 
-    indices.push_back(Tupla3i(2,1,6)); 
-    indices.push_back(Tupla3i(1,6,5)); 
+    indices.push_back(Tupla3i(7,4,3));
+    indices.push_back(Tupla3i(3,4,0));
+    indices.push_back(Tupla3i(0,1,2));
+    indices.push_back(Tupla3i(2,0,3));
+    indices.push_back(Tupla3i(3,2,6));
+    indices.push_back(Tupla3i(6,3,7));
+    indices.push_back(Tupla3i(7,6,5));
+    indices.push_back(Tupla3i(5,7,4));
+    indices.push_back(Tupla3i(4,5,1));
+    indices.push_back(Tupla3i(1,4,0));
+    indices.push_back(Tupla3i(2,1,6));
+    indices.push_back(Tupla3i(1,6,5));
 
 }
 
 Tetraedro::Tetraedro(){
-    // Inclusión de los vértices. 
-    vertices.push_back(Tupla3f(0,0,1)); 
-    vertices.push_back(Tupla3f(1,1,0)); 
-    vertices.push_back(Tupla3f(-1,1,0)); 
-    vertices.push_back(Tupla3f(0,-1,0)); 
+    // Inclusión de los vértices.
+    vertices.push_back(Tupla3f(0,0,1));
+    vertices.push_back(Tupla3f(1,1,0));
+    vertices.push_back(Tupla3f(-1,1,0));
+    vertices.push_back(Tupla3f(0,-1,0));
 
-    // Inclusión de los índices. 
+    // Inclusión de los índices.
     indices.push_back(Tupla3i(0,1,3));
     indices.push_back(Tupla3i(3,2,0));
     indices.push_back(Tupla3i(0,2,1));
