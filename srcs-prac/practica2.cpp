3cc96afba87fd9f949cc1cb73b418e4ea1d400e1
JCEntrena <jentrena@correo.ugr.es>
2015-11-09 20:02:04 +0200

    Añadido PLY para barrido y finalizada implementación de barridos.

--- srcs-prac/practica2.cpp
+++ srcs-prac/practica2.cpp
@@ -24,26 +24,40 @@ std::vector<MallaInd> figuras;
 // inicializado OpenGL.
 
 void P2_Inicializar( int argc, char *argv[] ){
-   // Para convertir char* en unsigned int
-   int n = atoi(argv[3]);
 
-   if (n < 3)
-      n = 3;
-
+   // Sin argumentos
    if (argc < 2){
       figuras.push_back(MallaPLY("../plys/beethoven.ply"));
       figuras.push_back(MallaRevol("../plys/peon.ply", 3));
+      figuras.push_back(MallaBarrido("../plys/cerrado.ply", 1, 1, 1, 3));
    }
-
-   else if (argc < 3){
+   // Se proporciona un PLY
+   else if (argc == 2){
       figuras.push_back(MallaPLY(argv[1]));
       figuras.push_back(MallaRevol("../plys/peon.ply", 3));
+      figuras.push_back(MallaBarrido("../plys/cerrado.ply", 1, 1, 1, 3));
    }
-   if (argc >= 4){
+   // Se da un PLY y una MallaRevol y el número de perfiles.
+   else if (argc == 4){
+      int n1 = atoi(argv[3]);
       figuras.push_back(MallaPLY(argv[1]));
-      figuras.push_back(MallaRevol(argv[2], n));
+      figuras.push_back(MallaRevol(argv[2], n1));
+      figuras.push_back(MallaBarrido("../plys/cerrado.ply", 1, 1, 1, 3));
    }
+   // Se dan un PLY, una MallaRevol con número de perfiles y una MallaBarrido con vector de desplazamiento y número de desplazamientos.
+   else if (argc == 8){
+      int n1 = atoi(argv[3]);
+      int n2 = atoi(argv[8]);
+      int x = atoi(argv[5]), y = atoi(argv[6]), z = atoi(argv[7]);
 
+      figuras.push_back(MallaPLY(argv[1]));
+      figuras.push_back(MallaRevol(argv[2], n1));
+      figuras.push_back(MallaBarrido(argv[4], x, y, z, n2));
+   }
+   else{
+      std::cout << "El número de argumentos introducido no es correcto.\n";
+      exit(-1);
+   }
 }
 
 // ---------------------------------------------------------------------
@@ -62,7 +76,7 @@ bool P2_FGE_PulsarTeclaNormal(unsigned char tecla){
 	  	objeto_activo_2 = (objeto_activo_2+1)%figuras.size();
       return true;
 	}
-   
+
 	return false;
 }
 
