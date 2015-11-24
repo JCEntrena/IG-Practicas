fa34bc392268472b3437704476e73bcd675c26c6
JCEntrena <jentrena@correo.ugr.es>
2015-11-24 14:02:33 +0200

    Cabeceras y archivo de implementación (vacío) de NodoGrafoEscena.

---
+++ srcs-prac/nodografoescena.cpp
@@ -0,0 +1,9 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 3  (implementación)
+// ** Implementación de la clase NodoGrafoEscena
+// **
+// *********************************************************************
+
+#include "nodografoescena.hpp"
---
+++ srcs-prac/nodografoescena.hpp
@@ -0,0 +1,43 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 3  (declaraciones públicas)
+// ** Archivos de la clase NodoGrafoEscena, derivada de Objeto3D,
+// ** para la creación de objetos jerárquicos.
+// **
+// *********************************************************************
+
+#include "Objeto3D.hpp"
+#include "MallaInd.hpp"
+#include "matrizg.hpp"
+#include <vector>
+
+using namespace std;
+
+// ---------------------------------------------------
+// Estructura de entrada del nodo del grafo de escena.
+struct EntradaNGE{
+   unsigned char tipoE;  // 0 = objeto. 1 = transformación.
+   union{
+      Objeto3D * objeto; // Puntero a un objeto.
+      Matriz4f * matriz; // Puntero a una matriz 4x4 (transformación).
+   };
+
+   // Constructores
+   EntradaNGE(Objeto3D * pObjeto); // Copia puntero.
+   EntradaNGE(const Matriz4f & pMatriz); // Crea copia de matriz.
+};
+
+// Clase NodoGrafoEscena
+class NodoGrafoEscena : public Objeto3D{
+   protected:
+      vector<EntradaNGE> entradas;
+   public:
+      // Método de visualizado.
+      virtual void visualizar(ContextoVis & cv);
+      // Añadir una entrada genérica.
+      void agregar(EntradaNGE * entrada);
+      // Construir una entrada y añadirla.
+      void agregar(Objeto3D * pObjeto);
+      void agregar(const Matriz4f & pMatriz); 
+}
