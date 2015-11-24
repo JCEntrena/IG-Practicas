6c7d2d933ae683d4883e5b62fb15a23fee46b4ea
JCEntrena <jentrena@correo.ugr.es>
2015-11-24 19:02:48 +0200

    :metal:

--- srcs-prac/practica3.hpp
+++ srcs-prac/practica3.hpp
@@ -10,9 +10,52 @@
 
 #include "MallaInd.hpp"
 #include "matrizg.hpp"
+#include "nodografoescena.hpp"
 
 void P3_Inicializar( int argc, char *argv[] ) ;
 bool P3_FGE_PulsarTeclaNormal(  unsigned char tecla ) ;
 void P3_DibujarObjetos( unsigned modo ) ;
+
+// -----------------------------------------------
+// Clases para los objetos de nuestro diseño.
+
+// Clase Cubo. Lee un cubo a partir de un PLY. Será el objeto que utilicemos para modelar todo nuestro diseño.
+class Cubo : public class NodoGrafoEscena{
+   public:
+      Cubo(){
+         MallaPLY *cubo = new MallaPLY("../plys/beethoven.ply");
+         this.agregar(cubo);
+      }
+}
+
+class Brazo : public class NodoGrafoEscena{
+   public:
+      Brazo(){
+
+      }
+
+}
+
+class Pierna : public class NodoGrafoEscena{
+   public:
+      Pierna(){
+
+      }
+
+}
+
+class Cabeza : public class NodoGrafoEscena{
+   public:
+       Cabeza(){
+          
+       }
+}
+
+class Tronco : public class NodoGrafoEscena{
+   public:
+      Tronco(){
+
+      }
+}
 
 #endif
