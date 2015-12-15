5f15b49d038cd29e89c2f64b7367f0adfd61dd93
JCEntrena <jentrena@correo.ugr.es>
2015-12-15 21:37:10 +0200

    :octocat:

--- srcs-prac/practica3.hpp
+++ srcs-prac/practica3.hpp
@@ -11,6 +11,7 @@
 #include "MallaInd.hpp"
 #include "matrizg.hpp"
 #include "nodografoescena.hpp"
+#include "matrices-tr.hpp"
 
 void P3_Inicializar( int argc, char *argv[] ) ;
 bool P3_FGE_PulsarTeclaNormal(  unsigned char tecla ) ;
@@ -20,42 +21,94 @@ void P3_DibujarObjetos( unsigned modo ) ;
 // Clases para los objetos de nuestro diseño.
 
 // Clase Cubo. Lee un cubo a partir de un PLY. Será el objeto que utilicemos para modelar todo nuestro diseño.
-class Cubo : public class NodoGrafoEscena{
+class Cubo : public NodoGrafoEscena{
    public:
       Cubo(){
          MallaPLY *cubo = new MallaPLY("../plys/beethoven.ply");
-         this.agregar(cubo);
+         agregar(cubo);
       }
-}
+};
 
-class Brazo : public class NodoGrafoEscena{
+class Brazo : public NodoGrafoEscena{
    public:
       Brazo(){
+         agregar(MAT_Escalado(1.2, 1.0, 2.5));
+         agregar(new Cubo());
+      }
+};
+
+class BrazoDerecho : public NodoGrafoEscena{
+   public:
+      BrazoDerecho(){
+         agregar(MAT_Traslacion(3.1, 0.0, 4.5));
+         agregar(MAT_Rotacion(0, 0, 1, 0));
+         agregar(new Brazo());
+      }
+      void rotar(bool sentido_horario){
 
       }
+};
 
-}
+class BrazoIzquierdo : public NodoGrafoEscena{
+   public:
+      BrazoIzquierdo(){
+         agregar(MAT_Traslacion(-1.3, 0.0, 4.5));
+         agregar(MAT_Rotacion(0, 0, 1, 0));
+         agregar(new Brazo());
+      }
+};
 
-class Pierna : public class NodoGrafoEscena{
+class PiernaIzquierda : public NodoGrafoEscena{
    public:
       Pierna(){
-
+         agregar(MAT_Escalado(1.2, 1.0, 2.9));
+         agregar(MAT_Rotacion(0, 0, 1, 0));
+         agregar(new Cubo());
       }
 
-}
+};
 
-class Cabeza : public class NodoGrafoEscena{
+class Cabeza : public NodoGrafoEscena{
    public:
        Cabeza(){
-          
+          agregar(MAT_Escalado(2, 2, 2))
+          agregar(MAT_Traslacion(0.5, -0.5, 8.1));
+          agregar(new Cubo());
        }
-}
+};
 
-class Tronco : public class NodoGrafoEscena{
+class Tronco : public NodoGrafoEscena{
    public:
       Tronco(){
-
+         agregar(MAT_Escalado(3.0, 3.0, 5.0));
+         agregar(MAT_Traslacion(0.0, -1.0, 3.0));
+         agregar(new Cubo());
       }
-}
+};
+
+class Caja : public NodoGrafoEscena{
+   private:
+      int desplazamiento;
+   public:
+      Caja(){
+         desplazamiento = 0;
+         agregar(MAT_Escalado(2.0, 2.0, 2.0));
+         agregar(MAT_Traslacion(-0.4, 1.2, 0.0));
+         agregar(MAT_Traslacion(0.0, 0.0, 0.0));
+         agregar(new Cubo());
+      }
+      void desplazar_adelante(){
+         if (desplazamiento < 10){
+            desplazamiento++;
+            entradas[2] = MAT_Traslacion(0.0, desplazamiento, 0.0);
+         }
+      }
+      void desplazar_atras(){
+         if (desplazamiento > 0){
+            desplazamiento--;
+            entradas[2] = (MAT_Traslacion(0.0, desplazamiento, 0.0));
+         }
+      }
+};
 
 #endif
