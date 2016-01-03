66e654b89a99817fca584b37ef29732f9e436918
JCEntrena <jentrena@correo.ugr.es>
2016-01-03 19:48:11 +0200

    Versión inicial de los archivos de la práctica 4, sin implementaciones. Obtenidos de los archivos de la P3.

---
+++ srcs-prac/practica4.cpp
@@ -0,0 +1,90 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 4 (implementación)
+// **
+// *********************************************************************
+
+#include "aux.hpp"
+#include "tuplasg.hpp"   // Tupla3f
+#include "practica4.hpp"
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+unsigned objeto_activo_3 = 0 ; // Objeto activo
+unsigned p4_grado_libertad_activo = 0; // Grado de libertad activo
+unsigned grados_libertad = 2; // Ángulos alpha y beta de la fuente de luz direccional
+
+// ---------------------------------------------------------------------
+// Declaraciones de estructuras de datos...
+
+Figura* figur;
+
+// ---------------------------------------------------------------------
+// Función para implementar en la práctica para inicialización.
+// Se llama una vez al inicio, cuando ya se ha creado la ventana e
+// inicializado OpenGL.
+
+void P4_Inicializar( int argc, char *argv[] ){
+
+}
+
+// ---------------------------------------------------------------------
+// Función invocada al pulsar una tecla con la práctica activa:
+// (si la tecla no se procesa en el 'main').
+//
+//  - devuelve 'true' si la tecla se usa en esta práctica para cambiar
+//    entre objetos (cambia el valor de
+//    'objeto_activo').
+//  - devuelve 'false' si la tecla no se usa en esta práctica (no ha
+//    cambiado nada)
+
+
+// Función para aumentar el grado de libertad activo. Se llama al pulsar ">"
+void aumenta(){
+   switch (p4_grado_libertad_activo){
+
+      default: break;
+   }
+
+}
+
+// Función para aumentar el grado de libertad activo. Se llama al pulsar "<"
+void disminuye(){
+   switch (p4_grado_libertad_activo){
+
+      default: break;
+   }
+
+}
+
+
+bool P4_FGE_PulsarTeclaNormal(unsigned char tecla){
+
+   switch (toupper(tecla)){
+      case 'G' :
+         p4_grado_libertad_activo = (p4_grado_libertad_activo+1)%grados_libertad;
+         switch(p4_grado_libertad_activo){
+
+            default: break;
+         }
+	      return true;
+      case '<' :
+         disminuye();
+         return true;
+      case '>' :
+         aumenta();
+         return true;
+      default :
+	     return false;
+   }
+}
+
+// ---------------------------------------------------------------------
+// Función para dibujar los objetos, implementada en MallaInd.
+
+void P4_DibujarObjetos(unsigned modo){
+
+}
---
+++ srcs-prac/practica4.hpp
@@ -0,0 +1,22 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 4  (declaraciones públicas)
+// **
+// *********************************************************************
+
+#ifndef IG_PRACTICA4_HPP
+#define IG_PRACTICA4_HPP
+
+#include "MallaInd.hpp"
+#include "matrizg.hpp"
+#include "nodografoescena.hpp"
+#include "matrices-tr.hpp"
+
+void P4_Inicializar( int argc, char *argv[] ) ;
+bool P4_FGE_PulsarTeclaNormal(  unsigned char tecla ) ;
+void P4_DibujarObjetos( unsigned modo ) ;
+
+// -----------------------------------------------
+
+#endif
