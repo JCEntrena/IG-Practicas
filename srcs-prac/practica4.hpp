fd4415ba3b5831c25179c01012943afb493cdfec
JCEntrena <jentrena@correo.ugr.es>
2016-01-20 02:09:16 +0200

    Minor changes.

--- srcs-prac/practica4.hpp
+++ srcs-prac/practica4.hpp
@@ -23,20 +23,6 @@ void P4_DibujarObjetos( unsigned modo ) ;
 // -----------------------------------------------
 // Clases adicionales
 
-// Clase Textura.
-class Textura{
-   public:
-      GLuint id_text;            // identificador de la textura
-      jpg::Imagen * img;         // puntero a imagen con los texels
-      unsigned mgct;             // modo de gen. cc.tt. (0 = desactivado, 1 = objeto, 2 = camara)
-      float cs[4], ct[4];        // coeficientes para generación de coords. de textura
-
-      void activar();            // activa textura en el cauce fijo de OpenGL
-      Textura(const std::string & archivoJPG);  // crea un textura a partir de un archivo
-};
-
-
-
 // Clases para las fuentes de luz.
 class FuenteLuz{
    public:
