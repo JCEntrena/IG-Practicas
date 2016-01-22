c4e84a4a914c1b40fa84d4f71a53a0b9a1b393b1
JCEntrena <jentrena@correo.ugr.es>
2016-01-22 22:13:01 +0200

    Minor changes.

--- srcs-prac/Material.hpp
+++ srcs-prac/Material.hpp
@@ -16,12 +16,41 @@ class Material{
 class MaterialEstandar : public Material{
    public:
       Textura * text;      // puntero a la textura (NULL en caso de no haber)
-      Tupla4f color[4];    // colores: 0: emisión, 1: ambiental, 2: difuso, 3: especular
+      Tupla3f color[4];    // colores: 0: emisión, 1: ambiental, 2: difuso, 3: especular
       float exponente;     // para la componente pseudo-especular
 
       virtual void activar();       // activa material en cauce fijo
                                     // si tiene textura se usa text
                                     // si no tiene textura se desactiva el uso de texturas
+};
+
+// Clases específicas de cada material.
+
+// Textura Coca Cola
+class MaterialLata : public MaterialEstandar{
+  public:
+    MaterialLata();
+};
+
+class MaterialTapasLata : public MaterialEstandar{
+  public:
+    MaterialTapasLata();
+};
+
+// Textura madera.
+class MaterialPeonMadera : public MaterialEstandar{
+  public:
+    MaterialPeonMadera();
+};
+
+class MaterialPeonBlanco : public MaterialEstandar{
+  public:
+    MaterialPeonBlanco();
+};
+
+class MaterialPeonNegro : public MaterialEstandar{
+  public:
+    MaterialPeonNegro();
 };
 
 #endif
